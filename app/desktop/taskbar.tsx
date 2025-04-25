86b30c3d4bc653ce9080174de2a4ab35a8761c85
Renier Jardio <renierjardio@gmail.com>
2025-04-25 11:50:32 +0800

    added assets, changed desktop bg, changes to taskbar

--- app/desktop/taskbar.tsx
+++ app/desktop/taskbar.tsx
@@ -1,91 +1,194 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {OpenCamera, OpenFileManager, OpenNote, OpenSettings, OpenSpotify, OpenChess} from "@/app/desktop/programOpener";
-import {FaGear, FaNoteSticky, FaSpotify} from "react-icons/fa6";
-import {FaCamera, FaFolder, FaChess} from "react-icons/fa";
-import {OpenedWindowsContext} from "@/app/context/openedWindowsContext";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  OpenCamera,
+  OpenFileManager,
+  OpenNote,
+  OpenSpotify,
+  OpenChess,
+} from "@/app/desktop/programOpener";
+import { FaSpotify } from "react-icons/fa6";
+import { FaCamera, FaFolder, FaChess, FaMicrophoneAlt } from "react-icons/fa";
+import Icon from "@mdi/react";
+import { mdiNoteText, mdiCalendarClock } from "@mdi/js";
+import { OpenedWindowsContext } from "@/app/context/openedWindowsContext";
 import useFileSystem from "@/hooks/useFileSystem";
 
 export default function Taskbar() {
-    const [currentTime, setCurrentTime] = useState('');
-    const [currentDate, setCurrentDate] = useState('');
-    const {openedWindows, setOpenedWindows} = useContext(OpenedWindowsContext);
-    const {directory, honey_directory} = useFileSystem();
-    // Update the current date and time every second
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const now = new Date();
-            const date = now.toLocaleDateString();
-            const time = now.toLocaleTimeString();
-            setCurrentTime(`${time}`);
-            setCurrentDate(`${date}`)
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+  const { openedWindows, setOpenedWindows } = useContext(OpenedWindowsContext);
+  const { directory, honey_directory } = useFileSystem();
 
-    return (
-        <div
-            className="text-sm flex items-center justify-center font-consolas space-x-1 text-white w-[100vw] bg-primary/80 h-[6vh]">
-            <div className="absolute right-0 pr-[1vw] ">
-                {currentTime}<br></br>
-                {currentDate}
-            </div>
-            <div className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300 rounded-md 
-            ${openedWindows[0].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenNote({
-                    openedWindows,
-                    setOpenedWindows,
-                }, {
-                    name: "untitled.txt",
-                    content: "",
-                    location: directory(),
-                })}>
-                <FaNoteSticky size={30} color={'yellow'}/>
-            </div>
-            <div
-                className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300 
-                rounded-md ${openedWindows[1].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenSettings({
-                    openedWindows,
-                    setOpenedWindows,
-                })}>
-                <FaGear size={30} color={'yellow'}/>
-            </div>
-            <div
-                className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300 
-                rounded-md ${openedWindows[2].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenCamera({
-                    openedWindows,
-                    setOpenedWindows,
-                })}>
-                <FaCamera size={30} color={'yellow'}/>
-            </div>
-            <div
-                className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300
-                rounded-md ${openedWindows[3].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenFileManager({
-                    openedWindows,
-                    setOpenedWindows,
-                })}>
-                <FaFolder size={30} color={'yellow'}/>
-            </div>
-            <div
-                className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300
-                rounded-md ${openedWindows[4].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenSpotify({
-                    openedWindows,
-                    setOpenedWindows,
-                })}>
-                <FaSpotify size={30} color={'yellow'}/>
-            </div>
-            <div
-                className={`p-3 cursor-pointer hover:bg-gray-700 hover:text-white transition-colors duration-300
-                rounded-md ${openedWindows[5].html ? 'bg-gray-700 ' : ''}`}
-                onClick={() => OpenChess({
-                    openedWindows,
-                    setOpenedWindows,
-                })}>
-                <FaChess size={30} color={'yellow'}/>
-            </div>
-        </div>
-    );
+  const [isHoveringMic, setIsHoveringMic] = useState(false);
+  const [isHoveringNote, setIsHoveringNote] = useState(false);
+  const [isHoveringFolder, setIsHoveringFolder] = useState(false);
+  const [isHoveringTask, setIsHoveringTask] = useState(false);
+  const [isHoveringCamera, setIsHoveringCamera] = useState(false);
+  const [isHoveringSpotify, setIsHoveringSpotify] = useState(false);
+  const [isHoveringChess, setIsHoveringChess] = useState(false);
+
+  // Update the current date and time every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const date = now.toLocaleDateString();
+      const time = now.toLocaleTimeString();
+      setCurrentTime(`${time}`);
+      setCurrentDate(`${date}`);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="text-sm flex items-center justify-center font-consolas space-x-1 gap-5 text-white w-[100vw] h-[10vh]">
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringMic
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringMic(true)}
+        onMouseLeave={() => setIsHoveringMic(false)}
+      >
+        <FaMicrophoneAlt size={30} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringNote
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringNote(true)}
+        onMouseLeave={() => setIsHoveringNote(false)}
+        onClick={() =>
+          OpenNote(
+            {
+              openedWindows,
+              setOpenedWindows,
+            },
+            {
+              name: "untitled.txt",
+              content: "",
+              location: directory(),
+            }
+          )
+        }
+      >
+        <Icon path={mdiNoteText} size={1.5} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringFolder
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringFolder(true)}
+        onMouseLeave={() => setIsHoveringFolder(false)}
+        onClick={() =>
+          OpenFileManager({
+            openedWindows,
+            setOpenedWindows,
+          })
+        }
+      >
+        <FaFolder size={30} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringTask
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringTask(true)}
+        onMouseLeave={() => setIsHoveringTask(false)}
+      >
+        <Icon path={mdiCalendarClock} size={1.5} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringCamera
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringCamera(true)}
+        onMouseLeave={() => setIsHoveringCamera(false)}
+        onClick={() =>
+          OpenCamera({
+            openedWindows,
+            setOpenedWindows,
+          })
+        }
+      >
+        <FaCamera size={30} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringSpotify
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringSpotify(true)}
+        onMouseLeave={() => setIsHoveringSpotify(false)}
+        onClick={() =>
+          OpenSpotify({
+            openedWindows,
+            setOpenedWindows,
+          })
+        }
+      >
+        <FaSpotify size={30} color={"white"} />
+      </div>
+
+      <div
+        className={`flex w-20 h-20 cursor-pointer transition-all duration-300 rounded-md bg-center bg-no-repeat bg-contain items-center justify-center`}
+        style={{
+          backgroundImage: `url(${
+            isHoveringChess
+              ? "/revisedHoneyOS/flower-hover.png"
+              : "/revisedHoneyOS/flower-normal.png"
+          })`,
+          backgroundSize: "contain",
+        }}
+        onMouseEnter={() => setIsHoveringChess(true)}
+        onMouseLeave={() => setIsHoveringChess(false)}
+        onClick={() =>
+          OpenChess({
+            openedWindows,
+            setOpenedWindows,
+          })
+        }
+      >
+        <FaChess size={30} color={"white"} />
+      </div>
+    </div>
+  );
 }
